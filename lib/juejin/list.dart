991ca3be5f272511f7f4bfa81c30291d7d5a2a9f
zx0286 <1061471799@qq.com>
2024-04-29 14:04:04 +0800

    1

--- lib/juejin/list.dart
+++ lib/juejin/list.dart
@@ -1,51 +1,26 @@
 import 'package:flutter/material.dart';
 
-import '../http.dart';
-
 class JueJinMainPage extends StatelessWidget {
   const JueJinMainPage({super.key, required this.title});
 
   final String title;
 
-  void handleTap(BuildContext context, int id) {
-    axios.get('/posts/$id').then((value) => {
-          Navigator.push(context, detail(value.data)),
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    return ListView.separated(
       itemCount: 20,
       itemBuilder: (context, i) {
-        return GestureDetector(
-          onTap: () => handleTap(context, i),
-          child: Padding(
-            padding: const EdgeInsets.all(10),
-            child: Text('Row ${i + 1}'),
-          ),
+        return ListTile(
+          leading: const Text('leading'),
+          title: Text('Row ${i + 1}'),
+          subtitle: const Text('subtitle'),
+          trailing: const Icon(Icons.more),
         );
       },
+      separatorBuilder: (context, index) => const Divider(
+        thickness: 1,
+        height: 1,
+      ),
     );
-  }
-
-  MaterialPageRoute detail(Map info) {
-    return MaterialPageRoute(builder: (context) {
-      return Container(
-        color: Colors.white,
-        alignment: Alignment.center,
-        child: Card(
-          child: GridTile(
-            header: GridTileBar(
-              title: Text(info['title']),
-              subtitle: Text(info['id'].toString()),
-            ),
-            footer: Text(info['userId'].toString()),
-            child: Text(info['body']),
-          ),
-          // child: Text(info.toString()),
-        ),
-      );
-    });
   }
 }
