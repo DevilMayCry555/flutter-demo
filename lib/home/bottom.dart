96d8f1bcaed9ab8383bc7eb2cd087e974cfbe862
zx0286 <1061471799@qq.com>
2024-04-30 17:35:32 +0800

    1

--- lib/home/bottom.dart
+++ lib/home/bottom.dart
@@ -1,27 +1,32 @@
 import 'package:flutter/material.dart';
 
 class BottomBarWidget extends StatefulWidget {
-  const BottomBarWidget({super.key});
+  const BottomBarWidget(
+      {super.key, required this.onTap, required this.position});
+  final Function(int) onTap;
+  final int position;
 
   @override
   State<StatefulWidget> createState() => _BottomBarWidgetState();
 }
 
 class _BottomBarWidgetState extends State<BottomBarWidget> {
-  int _position = 0;
+  // int _position = 0;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       type: BottomNavigationBarType.fixed,
       onTap: (position) {
-        setState(() => _position = position);
+        // setState(() => _position = position);
+        widget.onTap(position);
       },
       selectedItemColor: Colors.blue, // 选中时的颜色
       unselectedItemColor: Colors.black, // 未选中时的颜色
       showSelectedLabels: true, // 选中的label是否展示
       showUnselectedLabels: true, // 未选中的label是否展示
-      currentIndex: _position,
+      // currentIndex: _position,
+      currentIndex: widget.position,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: '待办'),
         BottomNavigationBarItem(icon: Icon(Icons.whatshot), label: '历史'),
--- lib/home/index.dart
+++ lib/home/index.dart
@@ -13,6 +13,27 @@ class IndexPage extends StatefulWidget {
 }
 
 class _IndexPageState extends State<IndexPage> {
+  late PageController _pageController;
+
+  int _position = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(initialPage: _position);
+  }
+
+  void _onTap(int selectedIndex) {
+    _pageController.jumpToPage(selectedIndex);
+  }
+
+  void _onPageChanged(int index) {
+    // 页面改变时更新下标状态
+    setState(() {
+      _position = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,13 +43,22 @@ class _IndexPageState extends State<IndexPage> {
       ),
       body: Container(
         color: Colors.white,
-        child: const Column(
-          children: [
-            ContentWidget(),
-          ], // 实例化这个搜索栏Widget
+        child: PageView(
+          controller: _pageController,
+          onPageChanged: _onPageChanged,
+          children: const [
+            Column(children: [ContentWidget()]),
+            Text('待办...'),
+            Text('发布...'),
+            Text('兑换...'),
+            Text('我的...'),
+          ],
         ),
       ),
-      bottomNavigationBar: const BottomBarWidget(),
+      bottomNavigationBar: BottomBarWidget(
+        onTap: _onTap,
+        position: _position,
+      ),
       drawer: Drawer(
         child: _leftDrawer(context),
       ),
