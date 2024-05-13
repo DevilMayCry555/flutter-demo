358e145afa1093c5eb910b92bb4e69d72fbbbbaf
zx0286 <1061471799@qq.com>
2024-05-13 17:03:28 +0800

    1

--- lib/home/bottom.dart
+++ lib/home/bottom.dart
@@ -1,17 +1,10 @@
 import 'package:flutter/material.dart';
 
-class BottomBarWidget extends StatefulWidget {
+class BottomBarWidget extends StatelessWidget {
   const BottomBarWidget(
       {super.key, required this.onTap, required this.position});
   final Function(int) onTap;
   final int position;
-
-  @override
-  State<StatefulWidget> createState() => _BottomBarWidgetState();
-}
-
-class _BottomBarWidgetState extends State<BottomBarWidget> {
-  // int _position = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -19,14 +12,14 @@ class _BottomBarWidgetState extends State<BottomBarWidget> {
       type: BottomNavigationBarType.fixed,
       onTap: (value) {
         // setState(() => _position = position);
-        widget.onTap(value);
+        onTap(value);
       },
       selectedItemColor: Colors.blue, // 选中时的颜色
       unselectedItemColor: Colors.black, // 未选中时的颜色
       showSelectedLabels: true, // 选中的label是否展示
       showUnselectedLabels: true, // 未选中的label是否展示
       // currentIndex: _position,
-      currentIndex: widget.position,
+      currentIndex: position,
       items: const [
         BottomNavigationBarItem(icon: Icon(Icons.article), label: '待办'),
         BottomNavigationBarItem(icon: Icon(Icons.history), label: '历史'),
