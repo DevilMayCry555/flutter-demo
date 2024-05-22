8dffb0ed11f690554236bb7405e5980ba956e949
zx0286 <1061471799@qq.com>
2024-05-22 16:51:06 +0800

    1

--- lib/location.dart
+++ lib/location.dart
@@ -1,11 +1,14 @@
 import 'http.dart';
 import 'package:geolocator/geolocator.dart';
 
+const isProd = bool.fromEnvironment('dart.vm.product');
 Future postLocation(String identity) async {
+  if (isProd) {
+    return Future.delayed(const Duration(seconds: 1));
+  }
   Position location = await Geolocator.getCurrentPosition(
     desiredAccuracy: LocationAccuracy.high,
   );
-
   var res = await axios.post(
     '/open',
     data: {
